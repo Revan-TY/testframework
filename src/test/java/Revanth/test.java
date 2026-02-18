a4fd05612e2efad5f820519cb0ae46ee7b61a19a
Admin <Admin@DESKTOP-IUVOT87>
2026-02-18 09:48:51 +0530

    Sample1

--- src/test/java/Revanth/test.java
+++ src/test/java/Revanth/test.java
@@ -1,5 +1,33 @@
 package Revanth;
 
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
 public class test {
+	
+	WebDriver driver;
+	
+	@BeforeMethod
+	public void Start()
+	{
+		driver = new ChromeDriver();
+		driver.get("Https://www.facebooc.com");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+	}
+	
+	@AfterMethod
+	public void end()
+	{
+		driver.quit();
+	}
+
+	
+	
 
 }
